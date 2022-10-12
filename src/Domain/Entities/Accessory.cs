e2e3e6fc6035a8214ff12d38c9428bb2a29a20e7
Jan Urbaś <janurbas39@gmail.com>
2022-10-12 07:56:40 +0200

    Add Vehicles property to define many-to-many relationships explicitly

--- src/Domain/Entities/Accessory.cs
+++ src/Domain/Entities/Accessory.cs
@@ -5,5 +5,6 @@ public class Accessory
     public Guid Id { get; set; }
     public Guid AccessoryCategoryId { get; set; }
     public AccessoryCategory AccessoryCategory { get; set; }
+    public ICollection<Vehicle> Vehicles { get; set; }
     public string Name { get; set; }
 }
